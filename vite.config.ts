c405d05785cd014bc865da2afa2ce51ae9af418e
Sadanya <2930048068@qq.com>
2025-01-01 11:37:07 +0800

    配置环境和打包

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,34 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": "/src",
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    base: env.VITE_BASE_DIR,
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': '/src'
+      }
     },
-  },
-});
+    build: {
+      outDir: 'docs',
+      rollupOptions: {
+        output: {
+          // 配置 js 最小拆包
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return id
+                .toString()
+                .split('node_modules/')[1]
+                .split('/')[0]
+                .toString()
+            }
+          }
+        }
+      }
+    }
+  }
+})
