5587001c9f7a81f188ce087de6456373029453f9
sadanya <2930048068@qq.com>
2025-02-24 18:04:21 +0800

    暂存 super-menu

---
+++ src/component/super-menu/super-menu.component.ts
@@ -0,0 +1,46 @@
+// import { Component, EventEmitter, Input, Output } from "@angular/core";
+
+// import { supermenu, SuperMenu } from "./super-menu.model";
+
+// @Component({
+//   selector: 'super-menu',
+//   templateUrl: './super-menu.component.html',
+//   styleUrls: ['./super-menu.component.css']
+// })
+// export class SuperMenuComponent {
+
+//   constructor() { }
+
+//   @Input() origin: SuperMenu[] = supermenu
+
+//   @Input() smenu: SuperMenu[] = supermenu
+
+//   @Output() selectmenu = new EventEmitter<SuperMenu>()
+
+//   select = (s: SuperMenu) => {
+//     this.clearAll(this.origin)
+//     s.select = true
+//     this.setSelect(this.origin)
+//     this.selectmenu.emit(s)
+//   }
+
+//   // 清除所有选中状态
+//   clearAll = (menu: SuperMenu[]) => {
+//     menu.forEach(v => {
+//       v.select = false
+//       if (v.children) this.clearAll(v.children)
+//     })
+//   }
+
+//   // 给一个树上的菜单项 select 属性赋值
+//   // 先递归到最底层, 再逐层往上, 如果子项有一项 select 为真, 则给父项也设为真
+//   setSelect = (menu: SuperMenu[]) => {
+//     menu.forEach(v => {
+//       if (v.children) {
+//         this.setSelect(v.children)
+//         v.select = !v.children.every(vv => vv.select == false)
+//       }
+//     })
+//   }
+
+// }
---
+++ src/component/super-menu/super-menu.model.ts
@@ -0,0 +1,57 @@
+// /**
+//  * 超级菜单数据格式
+//  * @param name 名称
+//  * @param level 层级
+//  * @param path 路由路径
+//  * @param open 是否展开
+//  * @param select 是否选中
+//  * @param children 子项, 拥有子项则说明存在子级菜单
+//  * @any [prop: string]: any 任意其他数据供使用者使用
+//  */
+//  export interface SuperMenu {
+//   name: string
+//   level: number
+//   path?: string
+//   open?: boolean
+//   select?: boolean
+//   children?: SuperMenu[]
+//   [prop: string]: any
+// }
+
+// export const supermenu: SuperMenu[] = [
+//   {
+//     name: '标题1',
+//     level: 1,
+//     path: './1'
+//   },
+//   {
+//     name: '标题2',
+//     level: 1,
+//     open: true,
+//     children: [
+//       { name: '标题2-1', level: 2 },
+//       { name: '标题2-2', level: 2 },
+//       { name: '标题2-3', level: 2 }
+//     ]
+//   },
+//   {
+//     name: '标题3',
+//     level: 1,
+//     open: true,
+//     children: [
+//       {
+//         name: '标题3-1',
+//         level: 2
+//       },
+//       {
+//         name: '标题3-2',
+//         level: 2,
+//         children: [
+//           { name: '标题3-2-1', level: 3, path: './3-2-1' },
+//           { name: '标题3-2-2', level: 3, path: './3-2-2' },
+//           { name: '标题3-2-3', level: 3, path: './3-2-3' }
+//         ]
+//       }
+//     ]
+//   }
+// ]
